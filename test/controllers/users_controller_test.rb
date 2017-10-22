132f6c99d0f99c4b9d33e5ca1de53ddc07ec4598
denys <1995gdv@gmail.com>
2017-10-22 22:36:31 +0300

    Adding partial of user data form

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -24,7 +24,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should redirect update when not logged in" do
-    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    patch :update, id: @user, user: {name: @user.name, email: @user.email}
     assert_not flash.empty?
     assert_redirected_to login_url
   end
@@ -38,9 +38,18 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
-    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    patch :update, id: @user, user: {name: @user.name, email: @user.email}
     assert flash.empty?
     assert_redirected_to root_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: {password: FILL_IN,
+                                           password_confirmation: FILL_IN,
+                                           admin: FILL_IN}
+    assert_not @other_user.FILL_IN.admin?
   end
 
   test "should redirect destroy when not logged in" do
