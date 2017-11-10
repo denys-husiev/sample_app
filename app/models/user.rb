44d7b8db6bb7b3b3af59ebb0bdf0d170d6a2b921
denys <1995gdv@gmail.com>
2017-11-10 11:43:20 +0200

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save {email.downcase!}
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
@@ -29,14 +30,39 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # Возвращает true, если предоставленный токен совпадает с дайджестом.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # Возвращает true, если данный токен совпадает с дайджестом.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Забывает пользователя
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Активирует аккаунт.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для активации.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  # Переводит адрес электронной почты в нижний регистр.
+  def downcase_email
+    self.email.downcase!
+  end
+
+  # Создает и присваивает активационнй токен и дайджест.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -46,10 +46,10 @@ class UsersControllerTest < ActionController::TestCase
   test "should not allow the admin attribute to be edited via the web" do
     log_in_as(@other_user)
     assert_not @other_user.admin?
-    patch :update, id: @other_user, user: {password: FILL_IN,
-                                           password_confirmation: FILL_IN,
-                                           admin: FILL_IN}
-    assert_not @other_user.FILL_IN.admin?
+    patch :update, id: @other_user, user: {password: "testtest",
+                                           password_confirmation: "testtest",
+                                           admin: true}
+    assert_not @other_user.admin?
   end
 
   test "should redirect destroy when not logged in" do
