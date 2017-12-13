3d6cf8831c6a61386a9863a89b61a89e577d53e0
denys <1995gdv@gmail.com>
2017-12-13 18:15:41 +0200

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
-  before_save   :downcase_email
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -44,13 +44,30 @@ class User < ActiveRecord::Base
 
   # Активирует аккаунт.
   def activate
-    update_attribute(:activated,    true)
+    update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
   end
 
   # Отправляет электронное письмо для активации.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Устанавливает атрибуты для сброса пароля.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для сброса пароля.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Возвращает true, если истек срок давности ссылки для сброса пароля .
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
@@ -62,7 +79,7 @@ class User < ActiveRecord::Base
 
   # Создает и присваивает активационнй токен и дайджест.
   def create_activation_digest
-    self.activation_token  = User.new_token
+    self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
 end
